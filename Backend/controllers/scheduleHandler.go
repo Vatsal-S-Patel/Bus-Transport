6dc64f41dfb3f584e3e6fc70f90c8af6d173c38c
manav vyas <manavzariya1@gmail.com>
2024-03-20 16:30:53 +0530

    now user can search source destination wich have no direct route

--- Backend/controllers/scheduleHandler.go
+++ Backend/controllers/scheduleHandler.go
@@ -69,3 +69,24 @@ func (c *Controller) GetUpcomingBus(w http.ResponseWriter, r *http.Request) {
 
 	OutputToClient(w, http.StatusOK, "schedule is fetched", ouput)
 }
+
+func (c *Controller) GetUpcomingSpecialBus(w http.ResponseWriter, r *http.Request) {
+	var variable map[string]int = map[string]int{}
+
+	err := json.NewDecoder(r.Body).Decode(&variable)
+	if err != nil {
+		OutputToClient(w, http.StatusInternalServerError, err.Error(), nil)
+		return
+	}
+
+	output, err := database.GetUpcomingSpecialBus(c.DB, variable["source"], variable["destination"])
+	if err != nil && err.Error() == "sorry no bus available" {
+		OutputToClient(w, http.StatusNotFound, err.Error(), nil)
+		return
+	} else if err != nil {
+		OutputToClient(w, http.StatusInternalServerError, err.Error(), nil)
+		return
+	}
+
+	OutputToClient(w, http.StatusOK, "schedule is fetched", output)
+}
