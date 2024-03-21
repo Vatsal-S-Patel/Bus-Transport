c34fcdf30dfb65238527013a1a1909f2e1127026
manav vyas <manavzariya1@gmail.com>
2024-03-21 11:07:10 +0530

    created prepare statement for fast query execution

--- Backend/database/routeQueries.go
+++ Backend/database/routeQueries.go
@@ -8,14 +8,14 @@ import (
 )
 
 func InsertRoute(db *sql.DB, route model.Route) error {
-	sqlStatement := `INSERT INTO transport.route (id, name, status, source, destination) VALUES ($1, $2, $3, $4, $5)`
-
-	_, err := db.Exec(sqlStatement, route.Id, route.Name, route.Status, route.Source, route.Destination)
+	sqlStatement,err:=db.Prepare(`INSERT INTO transport.route (id, name, status, source, destination) VALUES ($1, $2, $3, $4, $5)`)
 	if err != nil {
 		return err
 	}
+	defer sqlStatement.Close()
 
-	return nil
+	_, err = sqlStatement.Exec(route.Id, route.Name, route.Status, route.Source, route.Destination)
+	return err
 }
 
 func GetAllRoute(db *sql.DB) ([]model.Route, error) {
@@ -44,8 +44,17 @@ func GetAllRoute(db *sql.DB) ([]model.Route, error) {
 }
 
 func DeleteRoute(db *sql.DB, id string) error {
-	sqlStatement := `DELETE FROM transport.route WHERE id=$1`
-	sqlStatement2 := `DELETE FROM transport.routestations WHERE route_id=$1`
+	sqlStatement,err := db.Prepare(`DELETE FROM transport.route WHERE id=$1`)
+	if err != nil {
+		return err
+	}
+	defer sqlStatement.Close()
+
+	sqlStatement2,err := db.Prepare(`DELETE FROM transport.routestations WHERE route_id=$1`)
+	if err != nil {
+		return err
+	}
+	defer sqlStatement2.Close()
 
 	newId, err := strconv.Atoi(id)
 	if err != nil {
@@ -53,15 +62,11 @@ func DeleteRoute(db *sql.DB, id string) error {
 		return err
 	}
 
-	_, err = db.Exec(sqlStatement2, newId)
+	_, err = sqlStatement2.Exec(newId)
 	if err != nil {
 		return err
 	}
 
-	_, err = db.Exec(sqlStatement, newId)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	_, err = sqlStatement.Exec(newId)
+	return err
 }
